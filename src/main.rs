0582b1cdcb1516968e905eb8ba894a89e0dd95e4
Teddy Byron <ted@tedbyron.com>
2022-03-30 23:43:31 -0400

    impl single thread

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+#![warn(clippy::all, clippy::nursery, clippy::pedantic, rust_2018_idioms)]
+
+use std::fs;
+use std::io::{Read, Write};
+use std::net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream};
+
+const GET: &[u8] = b"GET / HTTP/1.1\r\n";
+
+fn main() {
+    let addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, 8080);
+    let socket = TcpListener::bind(addr).unwrap();
+
+    socket.incoming().filter_map(Result::ok).for_each(|stream| {
+        handle_stream(stream);
+    });
+}
+
+fn handle_stream(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let (status, html_path) = if buffer.starts_with(GET) {
+        ("HTTP/1.1 200 OK", "src/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "src/404.html")
+    };
+
+    let html = fs::read_to_string(html_path).unwrap();
+
+    let res = format!(
+        "{status}\r\nContent-Length: {len}\r\n\r\n{html}",
+        len = html.len(),
+    );
+
+    stream.write(res.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
